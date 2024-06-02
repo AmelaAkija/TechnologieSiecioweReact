c326653bced2e0b9b99aaee3198f7073a419d1ab
AmelaAkija <268306@student.pwr.edu.pl>
2024-06-02 19:42:25 +0200

    list 8

--- src/loan/loanService.ts
+++ src/loan/loanService.ts
@@ -1,12 +1,14 @@
+// loanService.ts
+
+import LoanType from './Loan'; // Assuming LoanType is defined in a separate file called types.ts
 import axios from 'axios';
-import Loan from './Loan';
 
-const API_URL = 'http://localhost:8080/Loan';
+const API_URL = 'http://localhost:8080/Loan'; // Assuming the endpoint for loans is http://localhost:8080/Loan
 
-export const fetchLoans = async (): Promise<Loan[]> => {
+export const fetchLoans = async (): Promise<LoanType[]> => {
   try {
     const response = await axios.get(`${API_URL}/GetAll`);
-    console.log('Response data:', response.data);
+    console.log('Response data:', response.data); // Add logging
     return response.data;
   } catch (error) {
     console.error('Error fetching loans:', error);
